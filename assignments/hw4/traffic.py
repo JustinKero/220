4ec879432e7fa46b19fe9feb53d4902f981f18e5
JustinKero <justin.kerosetz@gmail.com>
2021-09-30 00:49:45 -0400

    completed ch.4 assignment

--- assignments/hw4/traffic.py
+++ assignments/hw4/traffic.py
@@ -5,6 +5,7 @@ HW 4
 
 def main():
     road = eval(input("How many roads were surveyed?: "))
+    carnum = 0
     for i in range(road):
         day = str(i+1)
         days = eval(input("How many days was road " + day + " surveyed?: "))
@@ -13,6 +14,10 @@ def main():
             number = float(input("How many cars traveled on Day " + str(_+1) + "? "))
             total_sum += number
             avg = total_sum / days
+            carnum += number
+            caravg = carnum / road
         print("Road " + str(i+1), "average vehicles per day", avg)
+    print("Total number of vehicles on all roads: ", carnum)
+    print("Average number of vehicles per road: ", caravg)
 if __name__ == '__main__':
     main()
